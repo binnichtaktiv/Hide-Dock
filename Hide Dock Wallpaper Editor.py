6ea255901e1f9f512dd697a17a6cc3ff78182b89
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-08-14 09:07:29 +0200

    changed smth tmp file related and new ddl

--- Hide Dock Wallpaper Editor.py
+++ Hide Dock Wallpaper Editor.py
@@ -2,65 +2,39 @@ import cv2
 import numpy as np
 import requests
 import tempfile
+import os
 
-# Farbe definieren
-color = np.uint8([[[36, 36, 36]]]) # RGB-Wert von #242424
-tolerance = 20  # Toleranz für Farbauswahl
-
+color = np.uint8([[[36, 36, 36]]])
+tolerance = 20
 lower_color = np.clip(color - tolerance, 0, 255)
 upper_color = np.clip(color + tolerance, 0, 255)
-
-# URL des ursprünglichen Bildes
-original_image_url = 'https://hostigram.xyz/?tkn=7HYuqMs1vTgcW7OTQIDI2XpCqsuPRvMrRoVIyjb1TBKgo&dl=1'
+original_image_url = 'https://github.com/binnichtaktiv/Hide-Dock-Wallpaper-Editor/blob/main/Template.JPG?raw=true'
 
 print("Downloading original image...")
-# Bild herunterladen
 response = requests.get(original_image_url, stream=True)
 response.raise_for_status()
-with tempfile.NamedTemporaryFile(delete=True) as fp:
+temp_dir = tempfile.mkdtemp()
+original_image_path = os.path.join(temp_dir, 'temp_image.jpg')
+with open(original_image_path, 'wb') as fp:
     fp.write(response.content)
-    original_image_path = fp.name
 
-    print("Download complete. Processing image...")
-    # Bild laden
-    img = cv2.imread(original_image_path)
+print("Download complete. Processing image...")
+img = cv2.imread(original_image_path)
+proportion = 0.3
+crop_img = img[int(img.shape[0]*(1-proportion)):, :]
+mask = cv2.inRange(crop_img, lower_color, upper_color)
+cutout = cv2.bitwise_and(crop_img, crop_img, mask=mask)
+new_image_path = input("Enter the path to the new image here where you want to hide the dock:\n")
+output_image_path = input("Enter path to output folder here:\n")
+output_image_path = output_image_path + "/modified.png"
+new_img = cv2.imread(new_image_path)
+new_img_crop = new_img[int(new_img.shape[0]*(1-proportion)):, :]
+cutout = cv2.resize(cutout, (new_img_crop.shape[1], new_img_crop.shape[0]))
+mask = cv2.resize(mask, (new_img_crop.shape[1], new_img_crop.shape[0]))
+new_img_crop[mask != 0] = cutout[mask != 0]
+new_img[int(new_img.shape[0]*(1-proportion)):, :] = new_img_crop
 
-    # Proportion des unteren Teils des Bildes, der betrachtet werden soll
-    proportion = 0.3
-    crop_img = img[int(img.shape[0]*(1-proportion)):, :]
-
-    # Bereich mit der Farbe finden
-    mask = cv2.inRange(crop_img, lower_color, upper_color)
-
-    # Bereich ausschneiden
-    cutout = cv2.bitwise_and(crop_img, crop_img, mask=mask)
-
-    # Pfad zum neuen Bild
-    new_image_path = input("Enter the path to the new image here where you want to hide the dock:\n")
-
-    # Pfad zum Ausgabebild
-    output_image_path = input("Enter path to output folder here:\n")
-    output_image_path = output_image_path + "/modified.png"
-    # Neues Bild laden
-    new_img = cv2.imread(new_image_path)
-
-    # Unteren Teil des neuen Bildes schneiden
-    new_img_crop = new_img[int(new_img.shape[0]*(1-proportion)):, :]
-
-    # Skalieren des ausgeschnittenen Bereichs auf die Größe des neuen Bildes
-    cutout = cv2.resize(cutout, (new_img_crop.shape[1], new_img_crop.shape[0]))
-
-    # Skalieren der Maske auf die Größe des neuen Bildes
-    mask = cv2.resize(mask, (new_img_crop.shape[1], new_img_crop.shape[0]))
-
-    # Bereich auf neues Bild übertragen
-    new_img_crop[mask != 0] = cutout[mask != 0]
-
-    # Den unteren Teil des neuen Bildes ersetzen
-    new_img[int(new_img.shape[0]*(1-proportion)):, :] = new_img_crop
-
-    # Bild speichern
-    if cv2.imwrite(output_image_path, new_img):
-        print(f"Image saved to {output_image_path}")
-    else:
-        print(f"Failed to save image to {output_image_path}")
+if cv2.imwrite(output_image_path, new_img):
+    print(f"Image saved to {output_image_path}")
+else:
+    print(f"Failed to save image to {output_image_path}")
