c870518d74f76bf969f7ca8f2a2d82dd492869f2
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-09-27 22:10:35 +0200

    finally a working method

--- Hide Dock Wallpaper Editor.py
+++ Hide Dock Wallpaper Editor.py
@@ -1,40 +1,28 @@
-import cv2
-import numpy as np
-import requests
-import tempfile
 import os
+import tempfile
+import urllib.request
+from PIL import Image
 
-color = np.uint8([[[36, 36, 36]]])
-tolerance = 20
-lower_color = np.clip(color - tolerance, 0, 255)
-upper_color = np.clip(color + tolerance, 0, 255)
-original_image_url = 'https://github.com/binnichtaktiv/Hide-Dock-Wallpaper-Editor/blob/main/Template.JPG?raw=true'
+base_image_path = input("Enter the path to the new image here where you want to hide the dock:\n")
 
-print("Downloading original image...")
-response = requests.get(original_image_url, stream=True)
-response.raise_for_status()
-temp_dir = tempfile.mkdtemp()
-original_image_path = os.path.join(temp_dir, 'temp_image.jpg')
-with open(original_image_path, 'wb') as fp:
-    fp.write(response.content)
+base_image = Image.open(base_image_path)
 
-print("Download complete. Processing image...")
-img = cv2.imread(original_image_path)
-proportion = 0.3
-crop_img = img[int(img.shape[0]*(1-proportion)):, :]
-mask = cv2.inRange(crop_img, lower_color, upper_color)
-cutout = cv2.bitwise_and(crop_img, crop_img, mask=mask)
-new_image_path = input("Enter the path to the new image here where you want to hide the dock:\n")
-output_image_path = input("Enter path to output folder here:\n")
-output_image_path = output_image_path + "/modified.png"
-new_img = cv2.imread(new_image_path)
-new_img_crop = new_img[int(new_img.shape[0]*(1-proportion)):, :]
-cutout = cv2.resize(cutout, (new_img_crop.shape[1], new_img_crop.shape[0]))
-mask = cv2.resize(mask, (new_img_crop.shape[1], new_img_crop.shape[0]))
-new_img_crop[mask != 0] = cutout[mask != 0]
-new_img[int(new_img.shape[0]*(1-proportion)):, :] = new_img_crop
+overlay_image_url = "https://github.com/binnichtaktiv/Hide-Dock-Wallpaper-Editor/blob/main/Dock.PNG?raw=true"
+with urllib.request.urlopen(overlay_image_url) as response:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(response.read())
+        temp_path = tmp_file.name
 
-if cv2.imwrite(output_image_path, new_img):
-    print(f"Image saved to {output_image_path}")
-else:
-    print(f"Failed to save image to {output_image_path}")
+overlay_image = Image.open(temp_path)
+
+overlay_image = overlay_image.resize(base_image.size, Image.LANCZOS)
+
+combined_image = Image.alpha_composite(base_image.convert('RGBA'), overlay_image.convert('RGBA'))
+
+base_name, ext = os.path.splitext(base_image_path)
+output_image_path = f"{base_name}_hiddenDock{ext}"
+
+combined_image.save(output_image_path, 'PNG')
+print("Your new background picture with hidden dock should be here:" + output_image_path)
+
+os.unlink(temp_path)
