ec61c567a47e51f8cf176e0b2d7de0283e61ce78
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2024-10-26 14:39:45 +0200

    supports ios 18 dark mode now

--- Hide Dock Wallpaper Editor.py
+++ Hide Dock Wallpaper Editor.py
@@ -3,12 +3,20 @@ import tempfile
 import urllib.request
 from PIL import Image
 
+phoneAppearance = input("Which dark mode do you have activated?\n(1) normal dark mode\n(2) dark mode with dark icons\n")
 base_image_path = input("Enter the path to the new image here where you want to hide the dock:\n")
 
 base_image = Image.open(base_image_path)
 
-overlay_image_url = "https://github.com/binnichtaktiv/Hide-Dock-Wallpaper-Editor/blob/main/Dock.PNG?raw=true"
-with urllib.request.urlopen(overlay_image_url) as response:
+normalDarkDock = "https://github.com/binnichtaktiv/Hide-Dock/blob/main/normalDarkDock.PNG?raw=true"
+darkIconDarkModeDock = "https://github.com/binnichtaktiv/Hide-Dock/blob/main/darkIconDarkModeDock.png?raw=true"
+
+if phoneAppearance == '1':
+    imageUrl = normalDarkDock
+else:
+    imageUrl = darkIconDarkModeDock
+    
+with urllib.request.urlopen(imageUrl) as response:
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(response.read())
         temp_path = tmp_file.name
@@ -23,6 +31,6 @@ base_name, ext = os.path.splitext(base_image_path)
 output_image_path = f"{base_name}_hiddenDock{ext}"
 
 combined_image.save(output_image_path, 'PNG')
-print("Your new background picture with hidden dock should be here:" + output_image_path)
+print("Your new background picture with hidden dock should be here: " + output_image_path)
 
 os.unlink(temp_path)
